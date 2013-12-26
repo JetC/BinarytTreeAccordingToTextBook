3f07380671bfdd2c9f788366597b696a0fd76075
Sun PeiFeng <g7733pf@gmail.com>
2013-12-26 16:52:11 +0800

    查找被玩坏了

--- BinarytTreeAccordingToTextBook/main.c
+++ BinarytTreeAccordingToTextBook/main.c
@@ -18,81 +18,176 @@ typedef struct BinaryTreeNode{
     
 }BinaryTreeNode;
 
+//void destroyMyChildNodes(BinaryTreeNode *currentPosition);
+void visitThisBinaryTreeNodeData(BinaryTreeNode a);
+
+
 void binaryTreeInitiate(BinaryTreeNode **root)
 {
-    *root = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
     (*root)->leftChild = NULL;
     (*root)->rightChild = NULL;
 }
 
 BinaryTreeNode insertLeftChild(BinaryTreeNode *currentPosition,Datatype x)
 {
-    BinaryTreeNode *tempForLeftChild = NULL,*tempForSavingAddress = NULL;
+    BinaryTreeNode *tempForLeftChild = NULL;
     if (currentPosition == NULL)
     {
         exit(233);
     }
-    tempForSavingAddress = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
-    tempForSavingAddress = currentPosition->leftChild;
-    
     tempForLeftChild = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
     tempForLeftChild->data = x;
     tempForLeftChild->leftChild = NULL;
     tempForLeftChild->rightChild = NULL;
     
     currentPosition->leftChild = tempForLeftChild;
+    tempForLeftChild = NULL;
+    free(tempForLeftChild);
     return *(currentPosition->leftChild);
 }
 
 
 BinaryTreeNode insertRightChild(BinaryTreeNode *currentPosition,Datatype x)
 {
-    BinaryTreeNode *tempForRightChild, *tempForSavingAddress;
+    BinaryTreeNode *tempForRightChild;
     if (currentPosition == NULL)
     {
         exit(233);
     }
-    tempForSavingAddress = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
-    tempForSavingAddress = currentPosition->leftChild;
-    
+
     tempForRightChild = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
     tempForRightChild->data = x;
     tempForRightChild->leftChild = NULL;
     tempForRightChild->rightChild = NULL;
     
     currentPosition->rightChild = tempForRightChild;
+    tempForRightChild = NULL;
+    free(tempForRightChild);
     return *(currentPosition->rightChild);
 }
 
-BinaryTreeNode deleteLeftChild(BinaryTreeNode *currentPosition, Datatype x)
+BinaryTreeNode deleteLeftChild(BinaryTreeNode *currentPosition)
 {
     if (currentPosition == NULL || currentPosition->leftChild == NULL)
     {
         exit(234);
     }
-    
+    free(currentPosition->leftChild);
+    currentPosition->leftChild = NULL;
+    return *currentPosition;
 }
 
-void destroyMyLChildNodes(BinaryTreeNode *currentPosition)
+BinaryTreeNode deleteRightChild(BinaryTreeNode *currentPosition)
 {
-    BinaryTreeNode *p, *q;
-    p = currentPosition->leftChild;
-    while (p->leftChild != NULL)
+    if (currentPosition == NULL || currentPosition->rightChild == NULL)
     {
-        q = p->leftChild;
-        p->leftChild = NULL;
-//TODO:Start Here!
+        exit(234);
+    }
+    free(currentPosition->rightChild);
+    currentPosition->rightChild = NULL;
+    return *currentPosition;
+}
+
+void visitThisBinaryTreeNodeData(BinaryTreeNode a)
+{
+    printf("    %i\n",a.data);
+}
+
+void visitInPreOrder(BinaryTreeNode *t,void visitThisBinaryTreeNodeData(BinaryTreeNode item))
+{
+    if (t != NULL)
+    {
+        visitThisBinaryTreeNodeData(*t);
+        visitInPreOrder(t->leftChild, visitThisBinaryTreeNodeData);
+        visitInPreOrder(t->rightChild, visitThisBinaryTreeNodeData);
+
     }
     
 }
 
+void visitInInnerOrder(BinaryTreeNode *t, void visivisitThisBinaryTreeNodeData(BinaryTreeNode item))
+{
+    if (t != NULL)
+    {
+        visitInInnerOrder(t->leftChild, visitThisBinaryTreeNodeData);
+        visitThisBinaryTreeNodeData(*t);
+        visitInInnerOrder(t->rightChild, visivisitThisBinaryTreeNodeData);
+    }
+}
+
+void visitInPostOrder(BinaryTreeNode *t, void visivisitThisBinaryTreeNodeData(BinaryTreeNode item))
+{
+    if (t != NULL)
+    {
+        visitInPostOrder(t->leftChild,visitThisBinaryTreeNodeData);
+        visitInPostOrder(t->rightChild,visitThisBinaryTreeNodeData);
+        visitThisBinaryTreeNodeData(*t);
+    }
+
+}
+
+BinaryTreeNode * searchInBinaryTree(BinaryTreeNode *bt, Datatype x)
+{
+    BinaryTreeNode *p;
+    if (bt == NULL)
+    {
+        return NULL;
+    }
+    if (bt->data == x)
+    {
+        return  bt;
+    }
+    if (bt->leftChild != NULL)
+    {
+        p = searchInBinaryTree(bt->leftChild, x);
+        if (p != NULL)
+        {
+            return p;
+        }
+    }
+    if (bt->rightChild != NULL)
+    {
+        p = searchInBinaryTree(bt->leftChild, x);
+        if (p != NULL)
+        {
+            return p;
+        }
+    }
+    return NULL;
+}
+
+
+
 int main(int argc, const char * argv[])
 {
-    BinaryTreeNode *rootForBinaryTree;
-    rootForBinaryTree = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
-    binaryTreeInitiate(&rootForBinaryTree);
-    insertLeftChild(rootForBinaryTree, 5);
+    long val;
+    BinaryTreeNode *rootForBinaryTree;//建立根节点
+    rootForBinaryTree = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));//分配内存空间
+    binaryTreeInitiate(&rootForBinaryTree);//初始化子节点
     
+    insertLeftChild(rootForBinaryTree, 5);//子一级左节点
+    insertLeftChild(rootForBinaryTree->leftChild, 8);//子二级左节点
+    insertRightChild(rootForBinaryTree, 90);//子一级右节点
+//    deleteLeftChild(rootForBinaryTree->leftChild);
+//    deleteRightChild(rootForBinaryTree);
+//    visitThisBinaryTreeNodeData(*(rootForBinaryTree)->leftChild);
+    
+    printf("Visit in PRE order starts!\n");
+    visitInPreOrder(rootForBinaryTree, visitThisBinaryTreeNodeData );
+    printf("Visit in PRE order ends!\n\n");
+
+    printf("Visit in INNER order starts!\n");
+    visitInInnerOrder(rootForBinaryTree, visitThisBinaryTreeNodeData);
+    printf("Visit in INNER order ends!\n\n");
+
+    printf("Visit in POST order ends!\n");
+    visitInPostOrder(rootForBinaryTree,visitThisBinaryTreeNodeData);
+    printf("Visit in POST order ends!\n");
+
+    rootForBinaryTree->leftChild = searchInBinaryTree(rootForBinaryTree, 90);
+    
+    
+    free(rootForBinaryTree);
     return 0;
 }
 
